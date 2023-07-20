24189e870c70b3f03dbc4beae456471e83b0a252
MykhailoYavorskyi <my70035@gmail.com>
2023-07-20 22:16:24 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const listRef = document.querySelector(".gallery");
+
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"/>
+  </a>
+</li>`
+  )
+  .join("");
+listRef.insertAdjacentHTML("beforeend", markup);
+
+listRef.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+}
